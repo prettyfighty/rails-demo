fec273f26949a4e2f7f262cb2cfdcb6748f669a0
Lailai-lenovo <prettyfighty@gmail.com>
2021-03-29 17:25:12 +0800

    新增使用者初步介紹

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,17 @@
 class RestaurantsController < ApplicationController
 
-
     def index
     end
 
+    def new
+        # @restaurant = Restaurant.new
+    end
 
+    def list
 
+        # render html: "title is #{params[:title]}"
+        redirect_to '/restaurants'
+    end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  
   get '/restaurants', to: 'restaurants#index'
+  
+  get :new_restaurants, to: 'restaurants#new' #, path: "ccc"
+
+  post '/restaurants_list', to: 'restaurants#list'
+
 end
